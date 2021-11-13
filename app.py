0ed9e6490a851a544be1d848bf88554f8b2fbd47
amilanpathirana <emailpathirana@gmail.com>
2021-11-13 11:12:15 -0600

    images can be uploaded to MongoDB

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template,request
+from flask_wtf import FlaskForm
+from wtforms import FileField
+
 import pandas as pd
 import pickle as pickle
 from sklearn.preprocessing import LabelEncoder
@@ -6,8 +9,14 @@ import numpy as np
 import joblib
 from flask_pymongo import PyMongo
 import math
+from werkzeug.utils import secure_filename
+import os
 
-price_range=1000
+from savedvariables import *
+
+
+
+
 
 model1 = joblib.load('model.pkl')
 
@@ -16,21 +25,16 @@ mongo=PyMongo()
 
 
 
-MAKES = [
-    'honda',
-    'ford',
-    'toyota',
-    'nissan',
-    'mazda']
-
-
-MODELS = ['civic', 'cr-v', 'hr-v', 'escape', 'fiesta', 'edge', 'cx-3',
-          'cx-5', 'cx-7', 'corolla', 'rav4', 'highlander']
-YEARS = list(range(1995, 2021))
-MILEAGE = list(range(0, 500000, 10000))
-
 
 app = Flask(__name__)
+
+
+app.config['SECRET_KEY']='secretkey'
+
+class MyForm(FlaskForm):
+    image1=FileField('image1')
+    image2=FileField('image2')
+    image3=FileField('image3')
 
 app.config['MONGO_URI'] = 'mongodb+srv://amila:amila123@cluster0.wpswh.mongodb.net/mydb?retryWrites=true&w=majority'
 
@@ -44,7 +48,61 @@ def index():
 
 @app.route('/sell',methods=["GET","POST"])
 def sell():
-    return render_template('sell.html')
+    form=MyForm()
+    return render_template('sell.html',form=form,  makes=MAKES, models=MODELS, years=YEARS, mileage=MILEAGE)
+
+
+
+
+@app.route('/upload',methods=['GET','POST'])
+def upload():
+    
+    name= request.form.get('name')
+    email = request.form.get('email')
+    make = request.form.get('make')
+    model = request.form.get('model')
+    year = request.form.get('year')
+    km = request.form.get('km')
+    form=MyForm()
+
+    file1=request.files['image1']
+    file2=request.files['image2']
+    file3=request.files['image3']
+    
+
+    
+  
+    if not name:
+        return render_template('sell.html',form=form, message='Please Enter Your Name',makes=MAKES, models=MODELS, years=YEARS, mileage=MILEAGE)
+    if not email:
+        return render_template('sell.html',form=form, message='Please Enter Your Email',makes=MAKES, models=MODELS, years=YEARS, mileage=MILEAGE)
+    if not file1:
+        return render_template('sell.html',form=form, message='Please Submit Three Photos',makes=MAKES, models=MODELS, years=YEARS, mileage=MILEAGE)
+    if not file2:
+        return render_template('sell.html',form=form, message='Please Submit Three Photos',makes=MAKES, models=MODELS, years=YEARS, mileage=MILEAGE)
+    if not file3:
+        return render_template('sell.html',form=form, message='Please Submit Three Photos',makes=MAKES, models=MODELS, years=YEARS, mileage=MILEAGE)
+    if not make:
+        return render_template('sell.html',form=form,message='Please Enter Vehicle Make',makes=MAKES, models=MODELS, years=YEARS, mileage=MILEAGE)
+    if not model:
+        return render_template('sell.html',form=form,message='Please Enter Vehicle Model',makes=MAKES, models=MODELS, years=YEARS, mileage=MILEAGE)
+    if not year:
+        return render_template('sell.html',form=form,message='Please Enter Year of Manufacture',makes=MAKES, models=MODELS, years=YEARS, mileage=MILEAGE)
+    if not km:
+        return render_template('sell.html',form=form,message='Please Enter Vehicle Mileage',makes=MAKES, models=MODELS, years=YEARS, mileage=MILEAGE)
+
+    collection=mongo.db.useruploads 
+    
+    mongo.save_file(file1.filename,file1)
+    mongo.save_file(file2.filename,file2)
+    mongo.save_file(file3.filename,file3)
+    
+    collection.insert({'show': False ,'name': name, 'email':email, "make" : make, 'model':model,'year':year,'mileage':km,'image1_name':file1.filename,'image2_name':file2.filename,'image3_name':file3.filename})
+
+    return render_template('uploaded.html')
+
+
+
 
 
 @app.route('/calculate', methods=['POST','GET'])
@@ -55,6 +113,9 @@ def calculate():
     model = request.form.get('model')
     year = request.form.get('year')
     km = request.form.get('km')
+
+
+
 
     if not name:
         return render_template('index.html',message='Please Enter Name',makes=MAKES, models=MODELS, years=YEARS, mileage=MILEAGE)
@@ -75,6 +136,7 @@ def calculate():
         return render_template('index.html',message='Please Enter Mileage',makes=MAKES, models=MODELS, years=YEARS, mileage=MILEAGE)
 
 
+    
 
 
    
---
+++ savedvariables.py
@@ -0,0 +1,16 @@
+
+
+MAKES = [
+    'honda',
+    'ford',
+    'toyota',
+    'nissan',
+    'mazda']
+
+
+MODELS = ['civic', 'cr-v', 'hr-v', 'escape', 'fiesta', 'edge', 'cx-3',
+          'cx-5', 'cx-7', 'corolla', 'rav4', 'highlander']
+YEARS = list(range(1995, 2021))
+MILEAGE = list(range(0, 500000, 10000))
+
+price_range=1000
